c0ec177b3e1517d911dba486f62deb4a11308c77
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-05-20 15:36:29 +0100

    Simplified routing structure for better layout management

--- src/App.jsx
+++ src/App.jsx
@@ -14,55 +14,45 @@ import CheckOutPage from "./Pages/CheckOutPage";
 import Dashboard from "./Pages/dashboard";
 import AccountSettings from "./Pages/AccountSettings";
 import OrderDetails from "./Pages/OrderDetails";
-import Navbar from "./Components/layout/NavBar";
-import Footer from "./Components/layout/Footer";
-import Banner from "./Components/Banner/Banner";
 import ChangePassword from "./Pages/ChangePassword";
 import EditProfile from "./Pages/EditProfile";
 import FAQPage from "./Pages/Help";
+import Layout from "./Components/layout/Layout";
 import ProductDetails from "./Pages/ProductDetails";
-
-
-
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/">
-          <Route index element={<HomePage />} />
-          <Route path="all-dishes" element={<AllDishes />} />
-          <Route path="help" element={<FAQPage/>} />
-          <Route path="related-product" element={<RelatedProducts />} />
-          <Route path="product-details/:id" element={<ProductDetails />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route index element={<HomePage />} />
+        <Route path="all-dishes" element={<AllDishes />} />
+        <Route path="help" element={<FAQPage />} />
+        <Route path="related-product" element={<RelatedProducts />} />
+        <Route path="product-details/:id" element={<ProductDetails />} />
 
-
-          {/* Public Auth Routes */}
-          <Route path="auth">
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="forget-password" element={<ForgotPasswordPage />} />
-            <Route path="reset-password" element={<ResetPasswordPage />} />
-            <Route path="password-success" element={<PasswordSuccessPage />} />
-          </Route>
-
-          {/* Protected Routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="profile" element={<ProfilePage />} />
-            <Route path="edit-profile" element={<EditProfile />} />
-            <Route path="cart" element={<CartPage />} />
-            <Route path="checkout" element={<CheckOutPage />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="settings" element={<AccountSettings />} />
-            <Route path="/order/:orderId" element={<OrderDetails />} />
-            <Route path="change-password" element={<ChangePassword />}/>
-          </Route>
+        {/* Public Auth Routes */}
+        <Route path="auth">
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="forget-password" element={<ForgotPasswordPage />} />
+          <Route path="reset-password" element={<ResetPasswordPage />} />
+          <Route path="password-success" element={<PasswordSuccessPage />} />
         </Route>
-      </Routes>
-      <Banner />
-      <Footer />
-    </div>
+
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="edit-profile" element={<EditProfile />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="checkout" element={<CheckOutPage />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="settings" element={<AccountSettings />} />
+          <Route path="order/:orderId" element={<OrderDetails />} />
+          <Route path="change-password" element={<ChangePassword />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/Components/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './NavBar';
+import { Outlet } from 'react-router-dom';
+import Banner from '../Banner/Banner';
+import Footer from './Footer';
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Banner />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
